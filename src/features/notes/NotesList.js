1e7393484fd1dd8bb57a488a405927f8b63741d0
wosmateusz3@gmail.com <wosmateusz3@gmail.com>
2023-01-27 13:56:43 +0100

    add apiSlice for user and note fetch

--- src/features/notes/NotesList.js
+++ src/features/notes/NotesList.js
@@ -1,5 +1,54 @@
+import { useGetNotesQuery } from './notesApiSlice';
+import Note from './Note';
+
 const NotesList = () => {
-	return <h1>NotesList</h1>;
+	const {
+		data: notes,
+		isLoading,
+		isSuccess,
+		isError,
+		error,
+	} = useGetNotesQuery();
+
+
+	let content;
+	if (isLoading) content = <p>Loading...</p>;
+	if (isError) content = <p className='errmsg'>{error?.data?.message}</p>;
+	if (isSuccess) {
+		const { ids } = notes;
+		const tableContent = ids?.length
+			? ids.map((noteId) => <Note key={noteId} noteId={noteId} />)
+			: null;
+
+		content = (
+			<table className='table table--notes'>
+				<thead className='table__thead'>
+					<tr>
+						<th scope='col' className='table__th note__status'>
+							Status
+						</th>
+						<th scope='col' className='table__th note__created'>
+							Utworzone
+						</th>
+						<th scope='col' className='table__th note__updated'>
+							Zaaktualizowane
+						</th>
+						<th scope='col' className='table__th note__title'>
+							Tytuł
+						</th>
+						<th scope='col' className='table__th note__username'>
+							Utworzył
+						</th>
+						<th scope='col' className='table__th note__edit'>
+							Edytuj
+						</th>
+					</tr>
+				</thead>
+				<tbody>{tableContent}</tbody>
+			</table>
+		);
+	}
+	return content;
 };
 
 export default NotesList;
