8521fee8c74201066654696a4009ff20546260b6
wosmateusz3@gmail.com <wosmateusz3@gmail.com>
2023-01-30 13:31:25 +0100

    implement auth to components

--- src/features/auth/Login.js
+++ src/features/auth/Login.js
@@ -1,5 +1,102 @@
+import { useState, useRef, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { deploy } from '../../config/deploy';
+
+import { useDispatch } from 'react-redux';
+import { setCredentials } from './authSlice';
+import { useLoginMutation } from './authApiSlice';
+
 const Login = () => {
-	return <h1>Login</h1>;
+	const userRef = useRef();
+	const errRef = useRef();
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
+	const [errMsg, setErrMsg] = useState('');
+
+	const navigate = useNavigate();
+	const dispatch = useDispatch();
+
+	const [login, { isLoading }] = useLoginMutation();
+
+	useEffect(() => {
+		userRef.current.focus();
+	}, []);
+
+	useEffect(() => {
+		setErrMsg('');
+	}, [username, password]);
+
+	const errClass = errMsg ? 'errmsg' : 'offscreen';
+
+	const handleUserInput = (e) => setUsername(e.target.value);
+	const handlePwdInput = (e) => setPassword(e.target.value);
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		try {
+			const { accessToken } = await login({ username, password }).unwrap();
+			dispatch(setCredentials({ accessToken }));
+			setUsername('');
+			setPassword('');
+			navigate(`${deploy}/dash`);
+		} catch (err) {
+			if (!err.status) {
+				setErrMsg('Brak odpowiedzi servera');
+			} else if (err.status === 400) {
+				setErrMsg('Brak loginu lub hasła');
+			} else if (err.status === 401) {
+				setErrMsg('Niepoprawny login lub hasło');
+			} else {
+				setErrMsg(err.data?.message);
+			}
+			errRef.current.focus();
+		}
+	};
+
+	if (isLoading) {
+		return <p>Loading...</p>;
+	}
+
+	const content = (
+		<section className='public'>
+			<header>
+				<h1>Centrum Logowania</h1>
+			</header>
+			<main className='login'>
+				<p ref={errRef} className={errClass} aria-live='assertive'>
+					{errMsg}
+				</p>
+				<form className='form' onSubmit={handleSubmit}>
+					<label htmlFor='username'>Nazwa użytkownika</label>
+					<input
+						className='form__input'
+						type='text'
+						id='username'
+						ref={userRef}
+						value={username}
+						onChange={handleUserInput}
+						autoComplete='off'
+						required
+					/>
+					<label htmlFor='password'>Hasło:</label>
+					<input
+						className='form__input'
+						type='password'
+						id='password'
+						value={password}
+						onChange={handlePwdInput}
+						required
+					/>
+					<button className='form__submit-button'>Zaloguj się</button>
+				</form>
+			</main>
+			<footer>
+				<Link to={`/${deploy}`}>Strona główna</Link>
+			</footer>
+		</section>
+	);
+
+	return content;
 };
 
 export default Login;
